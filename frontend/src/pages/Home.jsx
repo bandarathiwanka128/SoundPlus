ee6631702dc408c7cbdc898bedba224e79db9f51
Thiwanka Bandara <thiwanka14535@gmail.com>
2026-02-13 20:27:36 +0530

    change wireless

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -52,7 +52,7 @@ const Home = () => {
 
   const categories = [
     {
-      name: 'Wireless Headphones',
+      name: 'WireleSsSS Headphones',
       image: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=400',
       link: '/products?category=wireless'
     },
